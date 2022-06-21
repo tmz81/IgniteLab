4ac1569a670afd39a6807d75cc3eecd506fe9f1e
tmz81 <tmz_81@hotmail.com>
2022-06-21 20:15:01 -0300

    Create struct sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-// define quais items serão puxados, qgl é para highlight
-const GET_LESSONS_QUERY = gql` 
-  query{
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
-
-interface LessonProps {
-  id: string,
-  title: string,
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-// Maneira simples para pega os dados da query, sem utilizar o useEffect  
-const { data } = useQuery<{lessons: LessonProps[] }>(GET_LESSONS_QUERY)
-  
   return (
-    <ul>{data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}</ul>
+    <div>
+     <Event />
+    </div>
   )
 }
