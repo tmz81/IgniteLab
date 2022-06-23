53a67ac98019baff7f75a734f2a07c81c4f8a3b6
tmz81 <tmz_81@hotmail.com>
2022-06-22 21:00:53 -0300

    💻🌎

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,7 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<h1>Homepage</h1>} />
-      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   )
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,14 +1,60 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
+
 import '@vime/core/themes/default.css'
 
-export function Video(){
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps){
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    }
+  })
+
+  if(!data) {
+    return <div className="flex-1">
+      <p>Carregando...</p>
+    </div>
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="qGLna1Uzpsw"/>
+            <Youtube videoId={data.lesson.videoId}/>
             <DefaultUi />
           </Player>
         </div>
@@ -18,24 +64,24 @@ export function Video(){
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 01 Abertura
+              {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa unde quis explicabo ab minima. Earum reprehenderit velit quae officia repellat, nulla harum numquam ipsa alias. Labore, numquam eveniet? Quos.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500" 
-                src="https://github.com/tmz81.png" 
+                src={data.lesson.teacher.avatarURL} 
                 alt="" />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Thomaz Victor
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  PRO @Skater
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
